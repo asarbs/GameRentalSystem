9590114705579f7d1b8f9abb6160c382aa957560
asar <bartosz@skorupa.net>
2017-05-27 21:07:47 +0200

    Simple Admin created.

--- ReservationSystemProject/ReservationSystemApp/admin.py
+++ ReservationSystemProject/ReservationSystemApp/admin.py
@@ -3,4 +3,31 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from models import Game
+from models import GameCopy
+from models import Event
+from models import Client
+
 # Register your models here.
+
+
+class GameCopyInline(admin.StackedInline):
+    model = GameCopy
+    extra = 1
+
+
+class GameAdmin(admin.ModelAdmin):
+    inlines = [GameCopyInline]
+
+
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+
+class ClientAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Game,GameAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Client, ClientAdmin)
