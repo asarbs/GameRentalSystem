42d131086ee0e5baf2a7febaa94f52877506aa36
asar <bartosz@skorupa.net>
2018-07-03 13:24:33 +0200

    Translate menu to polish.

--- ReservationSystemProject/ReservationSystemApp/templatetags/selected_event.py
+++ ReservationSystemProject/ReservationSystemApp/templatetags/selected_event.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django import template
 from django.core.urlresolvers import reverse
 from django.utils.safestring import mark_safe
@@ -13,15 +15,15 @@ def selected_event(context, request, user):
     try:
         event = Event.objects.get(id=request.session['event_id'])
     except KeyError:
-        return mark_safe(u'<a href="{0}" class="button">Select Event</a>'.format(reverse("SelectEvent", args=())))
-    return mark_safe(u'<b>Selected Event:</b> {}'.format(event))
+        return mark_safe(u'<a href="{0}" class="button">Wybierz wydarzenie</a>'.format(reverse("SelectEvent", args=())))
+    return mark_safe(u'<b>Wybrane wydarzenie:</b> {}'.format(event))
 
 
 @register.simple_tag(takes_context=True)
 def login(context, user):
     if user.is_anonymous():
         return mark_safe(u'<a href="{0}" class="button">Login</a>'.format(reverse("login", args=())))
-    return mark_safe(u'User: {} <a href="{}" class="button">Logout</a><a href="{}" class="button">Change password</a>'.
+    return mark_safe(u'User: {} <a href="{}" class="button">Logout</a><a href="{}" class="button">Zmień hasło</a>'.
                      format(user, reverse("logout"), reverse("changePassword") ))
 
 
@@ -33,20 +35,20 @@ def make_menu(context, user):
     menu_data = []
 
     if user.is_superuser:
-        menu_data.append([reverse("operatorList"), "Operator List"])
-        menu_data.append([reverse("ListEvent"), "Event List"])
-        menu_data.append([reverse("GamesList"), "Game List"])
+        menu_data.append([reverse("operatorList"), u'List operatorów'])
+        menu_data.append([reverse("ListEvent"), u'List wydarzeń'])
+        menu_data.append([reverse("GamesList"), u'Lista gier'])
 
-    menu_data.append([reverse("Rental"), "New Rental"])
-    menu_data.append([reverse("Return"), "Return"])
-    menu_data.append([reverse("RentalList"), "Rental List"])
-    menu_data.append([reverse("NewClient"), "New client"])
-    menu_data.append([reverse("ClientList"), "Client list"])
+    menu_data.append([reverse("Rental"), u'Nowe wyporzyczenie'])
+    menu_data.append([reverse("Return"), u'Zwrot egzemplarza gry'])
+    menu_data.append([reverse("RentalList"), u'Lista wyporzyczeń'])
+    menu_data.append([reverse("NewClient"), u'Nowy klient'])
+    menu_data.append([reverse("ClientList"), u'Lista klientów'])
 
-    menu_data.append([reverse("SelectEvent"), "Select Event"])
+    menu_data.append([reverse("SelectEvent"), u'Wybierz wydarzenie'])
 
     if user.is_superuser:
-        menu_data.append([reverse("statistics"), "Statistics"])
+        menu_data.append([reverse("statistics"), u'Statystyki'])
 
     menu = [u'<a href="{0}"  class="button">{1}</a>'.format(url[0], url[1]) for url in menu_data]
 
@@ -75,18 +77,18 @@ def menu2_Game(context, menu_data):
                  "/DeleteGame/",
                  "/GameCopyDetails/"]
     if any(s in path for s in urls_list):
-        menu_data.append([reverse("NewGame"), "New Game"])
+        menu_data.append([reverse("NewGame"), u'Nowa gra'])
 
 
 def menu2_Operator(context, menu_data):
     if context.request.get_full_path() == reverse("operatorList"):
-        menu_data.append([reverse("addOperator"), "New Operator"])
+        menu_data.append([reverse("addOperator"), u'Dodaj nowego operatora'])
 
 
 def menu2_Event(context, menu_data):
     path = context.request.get_full_path()
     urls_list = [reverse("ListEvent"), reverse("NewEvent"), "/EventUpdate/", "/EventDetails/"]
     if any(s in path for s in urls_list):
-        menu_data.append([reverse("NewEvent"), "New Event"])
+        menu_data.append([reverse("NewEvent"), u'Nowe wydarzenie'])
 
 
